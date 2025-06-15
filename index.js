344c83c5823487ceb164182e6591f16db9a8d86a
prajwal <spiderbasnet140@gmail.com>
2025-06-15 15:40:25 +0545

    some changes to algorithm

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-// fibonacci sequence 
-function fib(n){
-    let t = [];
-    t[0] = 0;
-    t[1] =1
-    if(n < 2 )return 1;
-    for (let i = 2; i < n; i++) {
-     t[i] = t[i-1] + t[i-2]
-        
-    }
-    return t;
+// fibonachi series
+
+function fibo(n){
+
 }
 
-console.log(fib(8));
+console.log(fibo(5))
--- mergeShort.js
+++ mergeShort.js
@@ -1,48 +1,41 @@
-// Merge short
-
-// [3, 2, 1, 13, 8, 5, 0, 1]  the output should be [0, 1, 1, 2, 3, 5, 8, 13]
 
 
-function mergeShort(arr, starting , highest){
-let finalData = []    
-let i = 0;
-let k =0;    
-let j = 0;
-
-if(starting < highest){
-    let midpoint = parseInt((starting + highest) /2)
-
-    mergeShort(arr,starting,midpoint)
-    mergeShort(arr,midpoint + 1 ,highest)
-
-    let leftHand = arr.slice(starting, midpoint + 1);
-    let rightHand = arr.slice(midpoint + 1, highest + 1);
-    while (i < leftHand.length && j < rightHand.length  ) {
-        
-        if(leftHand[i] < rightHand[j]){
-                finalData[k++] = leftHand[i++]
+function mergeShort(arr,start,end){
     
-            }else{
-                finalData[k++] = rightHand[j++]
-                
-            }
-        }
-        for(i; i < leftHand.length; i++){
-            finalData[k++] = leftHand[i]
-        }
-        for(j; j < rightHand.length; j++){
-            finalData[k++] = rightHand[j]
-        }
-        for (let m = 0; m < finalData.length; m++) {
-            arr[starting + m] = finalData[m];
+    if( start >= end) return 1;
+    let midvalue = Math.floor((start + end) / 2);
+
+    mergeShort(arr,start,midvalue);
+    mergeShort(arr,midvalue+1,end);
+    
+    let left = arr.slice(start,midvalue + 1);
+    let right = arr.slice(midvalue + 1, end + 1);
+
+    let i = 0;
+    let j = 0;
+    let k = start;
+
+    while (i < left.length && j < right.length) {
+
+        if(left[i] < right[j]){
+            console.log(left[i] ," --- ", right[j])
+            arr[k++] = left[i++]
+        }else{
+            console.log(left[i] ," *** ", right[j])
+            
+            arr[k++] = right[j++]
         }
     }
-    
-    // console.log(finalData)
+    while(i < left.length){
+        arr[k++] = left[i++]
+    }
+    while(j < right.length){
+        arr[k++] = right[j++]
+    }
+    return arr;
 }
 
 let myArr = [3, 2, 1, 13, 8, 5, 0, 1];
-mergeShort(myArr,0 ,myArr.length )
-console.log(myArr)
+console.log(mergeShort(myArr,0 ,myArr.length ))
 
 
